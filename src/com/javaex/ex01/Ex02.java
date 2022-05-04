c4520f03b037d3513ba316607c6a78058b7d6a37
최정필 <wjdvlf1407@gmail.com>
2022-05-04 12:11:54 +0900

    220504_파일인풋아웃풋스트림

--- src/com/javaex/ex01/Ex02.java
+++ src/com/javaex/ex01/Ex02.java
@@ -6,29 +6,30 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class Ex01 {
+public class Ex02 {
 
 	public static void main(String[] args) throws IOException {
 
 		InputStream in = new FileInputStream("/Users/choijungphil/javaStudy/file/img.jpg");
-		OutputStream out = new FileOutputStream("/Users/choijungphil/javaStudy/file/byteImg.jpg");
+		OutputStream out = new FileOutputStream("/Users/choijungphil/javaStudy/file/byteBuffImg.jpg");
+
+		byte[] buff = new byte[1024];
 
 		System.out.println("복사시작");
-
 		while (true) {
-			int data = in.read();
 
-			System.out.println("복사끝" + data);
+			int data = in.read(buff);
 			if (data == -1) {
+				System.out.println("복사끝" + data);
 				break;
 			}
-
-			out.write(data);
+			out.write(buff);
 
 		}
 
 		out.close();
 		in.close();
+
 	}
 
 }
