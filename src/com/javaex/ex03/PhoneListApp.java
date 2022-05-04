91caf6255b14a000a5e3907d29b0f224e184c062
최정필 <wjdvlf1407@gmail.com>
2022-05-04 16:57:03 +0900

    주석정리

--- src/com/javaex/ex03/PhoneListApp.java
+++ src/com/javaex/ex03/PhoneListApp.java
@@ -15,9 +15,11 @@ public class PhoneListApp {
 	public static void main(String[] args) throws IOException {
 
 		List<Person> pList = new ArrayList<Person>();
+		// PhoneDB_원본이란 이름의 데이터값 불러오기
 		Reader fr = new FileReader("/Users/choijungphil/javaStudy/file/PhoneDB_원본.txt");
 		BufferedReader br = new BufferedReader(fr);
 
+		// PhoneDB_복사본란 이름의 메모장 경로에 추가
 		Writer fw = new FileWriter("/Users/choijungphil/javaStudy/file/PhoneDB_복사본.txt");
 		BufferedWriter bw = new BufferedWriter(fw);
 
@@ -31,12 +33,15 @@ public class PhoneListApp {
 			bw.newLine();
 
 			String[] info = str.split(",");
+			// info[0] = 이름, info[1] = 휴대폰번호, info[2] = 회사번호
 			Person p = new Person(info[0], info[1], info[2]);
 
+			// 어레이리스트에 입력받은 값만큼 추가
 			pList.add(p);
 
 		}
 
+		// 어레이리스트의 정보 Person에서 불러와 출력
 		for (Person i : pList) {
 			i.showInfo();
 		}
